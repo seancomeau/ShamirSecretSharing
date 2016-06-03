74375672116118ad977c3d095d3496881ed7ee42
Mohamed Bamakhrama <mohamed@alumni.tum.de>
2016-06-03 16:12:30 +0200

    Fix setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,10 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
+with open('../README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('../LICENSE') as f:
     license = f.read()
 
 setup(
